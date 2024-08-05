57015298164ca2b0dce4249dfa0aa7f67293c2b6
GrumpyJesus <76903839+GrumpyJesus@users.noreply.github.com>
2024-08-05 11:08:27 +0200

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -79,8 +79,7 @@ const default_config = {
             url: "https://monkeytype.com/",
             icon: "keyboard",
             icon_color: "#e78a4e",
-            openInNewTab: true,
-          },
+            openInNewTab: true          },
           {
             name: "tetris",
             url: "https://tetris.com/",
